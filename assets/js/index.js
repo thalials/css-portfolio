5351106f5715067cb9a7906f8b022955337b717e
Thalia Loiola <thalials@al.insper.edu.br>
2019-09-17 18:02:10 -0300

    alterações no index

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   let db = coDesConnect('https://grupo-7-66c34.firebaseio.com/')
 
   db.download('/', function(data) {
-    console.log(data)
-
     context = data
     coDesReplace('.category-list', context)
+    coDesReplace('.menu-list', context)
 
   })
 })
