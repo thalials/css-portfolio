41d2922930bd94c862a495c2696814b1d59366ac
Thalia Loiola <thalials@al.insper.edu.br>
2019-09-17 18:06:19 -0300

    categoria

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -1,24 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let params = coDesExtract()
+	let params = coDesExtract()
   let value = params['id']
 
   let db = coDesConnect('https://grupo-7-66c34.firebaseio.com/')
 
   db.download('/', function(data) {
 
-
     context = data['portfolio'][value]
-    coDesReplace('.title', context)
+    coDesReplace('title', context)
 
     context = data
     coDesReplace('.menu-list', context)
 
     context = data['portfolio'][value]
-    coDesReplace('.projetos', context)
-
-    context = data['portfolio'][value]
     coDesReplace('.tudo', context)
 
+    context = data['portfolio'][value]
+    coDesReplace('.projetos', context)
   })
 })
-
