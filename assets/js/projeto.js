7741933fa5d63e2c43aac6488eeb358bfb20f0d5
Thalia Loiola <thalials@al.insper.edu.br>
2019-09-17 18:19:09 -0300

    mudancas projetos

--- assets/js/projeto.js
+++ assets/js/projeto.js
@@ -1,40 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 	let params = coDesExtract()
-  	let value1 = params['key1']
+  	let value1 = params['id']
   	let value2 = params['key2']
-
-
 
 	let db = coDesConnect('https://grupo-7-66c34.firebaseio.com/')
 
 	db.download('/', function(data) {
-    	console.log(data)
-
-    	context = data
+    	
+        context = data
+        coDesReplace('.menu-list', context)
+    	
+        context = data
     	coDesReplace('.projeto-title', context)
 
-    	context = data
-    	coDesReplace('.category-list', context)
-
-    	context = data
-    	coDesReplace('.menu-list', context)
-
-
-    	context = data
+    	context = data['portfolio']['categoria']['projeto'][value2]
     	coDesReplace('.cover', context)
-
 
     	context = data['portfolio']['categoria']['projeto'][value2]
     	coDesReplace('.cell', context)
-
-
-
-
-
-
-
-
-
-
-
-}
+        
+    })
+})
