1cc245e37900cce99bcf1c0ac88a5ae861ec2e20
Larissa Silva <larissaos@al.insper.edu.br>
2019-09-17 17:56:08 -0300

    imagens extras

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -1,31 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
+  let params = coDesExtract()
+  let value = params['key']
 
-	let params = coDesExtract()
-    let value = params['key']
+  let db = coDesConnect('https://grupo-7-66c34.firebaseio.com/')
 
-    let db = coDesConnect('https://grupo-7-66c34.firebaseio.com/')
-
-   db.download('/', function(data) {
-
-    for(let key in data) {
-
-      console.log(data[key])
+  db.download('/', function(data) {
 
 
-      db.download('/', function(data) {
+    context = data['portfolio'][value]
+    coDesReplace('.title', context)
 
-        context = data['portifolio'][value]
-        coDesReplace('title', context)
+    context = data
+    coDesReplace('.menu-list', context)
 
-        context = data['portifolio'][value]
-        coDesReplace('.menu-list', context)
+    
 
-        context = data['portifolio'][value]
-        coDesReplace('.projetos', context)
+    context = data['portfolio'][value]
+    coDesReplace('.tudo', context)
 
-        context = data['portifolio'][value]
-        coDesReplace('.list_projetos', context)
-
-    }
-}
+  })
 })
+
